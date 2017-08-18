ac3163f693180f3225afa4b57f9464c8113c241e
siva <sivaforum@gmail.com>
2017-08-18 12:05:29 -0500

    Added the sample Cordova plugin
    Added the sample Cordova plugin

---
+++ src/ios/storeapp.m
@@ -0,0 +1,28 @@
+/********* storeapp.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface storeapp : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation storeapp
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
