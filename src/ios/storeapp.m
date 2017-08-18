baa55b48e5b6d3afa549c53e9da87739241560f7
siva <sivaforum@gmail.com>
2017-08-18 16:23:20 -0500

    updated the function
    updated the function

--- src/ios/storeapp.m
+++ src/ios/storeapp.m
@@ -1,28 +1,60 @@
 /********* storeapp.m Cordova Plugin Implementation *******/
 
+#import "storeapp.h"
 #import <Cordova/CDV.h>
-
-@interface storeapp : CDVPlugin {
-  // Member variables go here.
-}
-
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
-@end
 
 @implementation storeapp
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
-{
+- (void)check:(CDVInvokedUrlCommand*)command {
+    
     CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    
+    NSString* scheme = [command.arguments objectAtIndex:0];
+    
+    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:scheme]]) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:(true)];
     }
-
+    else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:(false)];
+    }
+    
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    
+}
+
+- (void)start:(CDVInvokedUrlCommand*)command {
+    
+    CDVPluginResult* pluginResult = nil;
+    
+    NSString* scheme = [command.arguments objectAtIndex:0];
+    
+    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:scheme]]) {
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:scheme]];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:(true)];
+    }
+    else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:(false)];
+    }
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    
+}
+
+- (void)go:(CDVInvokedUrlCommand*)command {
+    
+    CDVPluginResult* pluginResult = nil;
+    
+    NSString* scheme = [command.arguments objectAtIndex:0];
+    
+    if ([[UIApplication sharedApplication] openURL:[NSURL URLWithString:scheme]]) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:(true)];
+    }
+    else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsBool:(false)];
+    }
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    
 }
 
 @end
