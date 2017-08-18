45701a54186b5e2cb4a4d6095e80de6d2d7d154d
siva <sivaforum@gmail.com>
2017-08-18 16:25:25 -0500

    Changed javascript file
    Changed javascript file

--- www/storeapp.js
+++ www/storeapp.js
@@ -1,5 +1,60 @@
+/**
+ * storeapp
+	
+	Phonegap plugin for check or launch other application in android device (iOS support).
+*/
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "storeapp", "coolMethod", [arg0]);
-};
+module.exports = {
+    /** 
+     * Set application params
+     *
+     * @param {Mixed} params				params, view documentation 
+     * @param {Mixed} extra   				Extra fields
+     * @param {Function} errorCallback		The callback that is called when an error occurred when the program starts.
+     *
+     */
+
+    set: function(params, extra) {
+        var output = [params];
+
+        if (extra != undefined) {
+            output.push(extra);
+        } else {
+            output.push(null);
+        }
+
+        return {
+            start: function(completeCallback, errorCallback) {
+                completeCallback = completeCallback || function() {};
+                errorCallback = errorCallback || function() {};
+
+                exec(completeCallback, errorCallback, "storeapp", "start", output);
+            },
+            check: function(completeCallback, errorCallback) {
+                completeCallback = completeCallback || function() {};
+                errorCallback = errorCallback || function() {};
+
+                exec(completeCallback, errorCallback, "storeapp", "check", output);
+            },
+            go: function(completeCallback, errorCallback) {
+                completeCallback = completeCallback || function() {};
+                errorCallback = errorCallback || function() {};
+
+                exec(completeCallback, errorCallback, "storeapp", "go", output);
+            }
+        }
+    },
+    /**
+     * extra values
+     */
+    getExtras: function(completeCallback, errorCallback) {
+        exec(completeCallback, errorCallback, "storeapp", "getExtras", []);
+    },
+    getExtra: function(extraValue, completeCallback, errorCallback) {
+        exec(completeCallback, errorCallback, "storeapp", "getExtra", [extraValue]);
+    },
+    hasExtra: function(extraValue, completeCallback, errorCallback) {
+        this.getExtra(extraValue, completeCallback, errorCallback);
+    }
+}
